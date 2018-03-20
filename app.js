601d90fc93a1e54c33975dd5e47f2bfa4f1cd40f
Juan Cabello <jcabelloc@gmail.com>
2018-03-20 10:23:25 -0500

    Adding Mongoose to the Project

--- app.js
+++ app.js
@@ -1,47 +1,79 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-app.use (bodyParser.urlencoded({extended: true}));
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelpDB");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    {name: "Salmon Creek", image: "https://onmilwaukee.com/images/articles/ca/camping/camping_fullsize_story1.jpg"},
-    {name: "Granite Hill", image: "https://www.visitnc.com/resimg.php/imgcrop/2/52908/image/800/448/KerrCamping.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://recreation-acm.activefederal.com/assetfactory.aspx?did=5221"},
-    {name: "Salmon Creek", image: "https://onmilwaukee.com/images/articles/ca/camping/camping_fullsize_story1.jpg"},
-    {name: "Granite Hill", image: "https://www.visitnc.com/resimg.php/imgcrop/2/52908/image/800/448/KerrCamping.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://recreation-acm.activefederal.com/assetfactory.aspx?did=5221"},
-    {name: "Salmon Creek", image: "https://onmilwaukee.com/images/articles/ca/camping/camping_fullsize_story1.jpg"},
-    {name: "Granite Hill", image: "https://www.visitnc.com/resimg.php/imgcrop/2/52908/image/800/448/KerrCamping.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://recreation-acm.activefederal.com/assetfactory.aspx?did=5221"},
-    {name: "Salmon Creek", image: "https://onmilwaukee.com/images/articles/ca/camping/camping_fullsize_story1.jpg"},
-    {name: "Granite Hill", image: "https://www.visitnc.com/resimg.php/imgcrop/2/52908/image/800/448/KerrCamping.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://recreation-acm.activefederal.com/assetfactory.aspx?did=5221"}
+//schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
 
-];
+var Campground = mongoose.model("Campground", campgroundSchema);
 
+/*
+Campground.create(
+    {
+        name: "Granite Hill", 
+        image: "https://www.visitnc.com/resimg.php/imgcrop/2/52908/image/800/448/KerrCamping.jpg",
+        description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word"
+    }, function(err, campground){
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Create Campground: " );
+            console.log(campground)
+        }
+    }
+);
+*/
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("index", {campgrounds: allCampgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    var description = req.body.description;
+    var newCampground = {name: name, image: image, description: description};
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res){
     res.render("new");
 });
 
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("show", {campground: foundCampground});
+        }
+    });
+});
 
 app.listen(3000, function(req, res){
     console.log("Yelp Camp Server has started!!!");
