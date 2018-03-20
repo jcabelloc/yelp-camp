9a53ab1b7fc61fb6b9ea7c055c44850707835c06
Juan Cabello <jcabelloc@gmail.com>
2018-03-19 23:52:11 -0500

    UI Improvements using BootStrap 4

--- app.js
+++ app.js
@@ -8,8 +8,19 @@ app.set("view engine", "ejs");
 var campgrounds = [
     {name: "Salmon Creek", image: "https://onmilwaukee.com/images/articles/ca/camping/camping_fullsize_story1.jpg"},
     {name: "Granite Hill", image: "https://www.visitnc.com/resimg.php/imgcrop/2/52908/image/800/448/KerrCamping.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://recreation-acm.activefederal.com/assetfactory.aspx?did=5221"},
+    {name: "Salmon Creek", image: "https://onmilwaukee.com/images/articles/ca/camping/camping_fullsize_story1.jpg"},
+    {name: "Granite Hill", image: "https://www.visitnc.com/resimg.php/imgcrop/2/52908/image/800/448/KerrCamping.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://recreation-acm.activefederal.com/assetfactory.aspx?did=5221"},
+    {name: "Salmon Creek", image: "https://onmilwaukee.com/images/articles/ca/camping/camping_fullsize_story1.jpg"},
+    {name: "Granite Hill", image: "https://www.visitnc.com/resimg.php/imgcrop/2/52908/image/800/448/KerrCamping.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://recreation-acm.activefederal.com/assetfactory.aspx?did=5221"},
+    {name: "Salmon Creek", image: "https://onmilwaukee.com/images/articles/ca/camping/camping_fullsize_story1.jpg"},
+    {name: "Granite Hill", image: "https://www.visitnc.com/resimg.php/imgcrop/2/52908/image/800/448/KerrCamping.jpg"},
     {name: "Mountain Goat's Rest", image: "https://recreation-acm.activefederal.com/assetfactory.aspx?did=5221"}
+
 ];
+
 
 app.get("/", function(req, res){
     res.render("landing");
