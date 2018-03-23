8957002b21db45bbb810d1aa130b2e64b7d07c82
Juan Cabello <jcabelloc@gmail.com>
2018-03-22 21:18:24 -0500

    Refactoring using  Module Exports

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose");
+    mongoose    = require("mongoose")
+    Campground  = require("./models/campground");
 
 mongoose.connect("mongodb://localhost/yelpDB");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-//schema setup
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
 
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 /*
 Campground.create(
