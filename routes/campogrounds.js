9d292f88443d7a0cf5f3d10ae2ebc83d70855130
Juan Cabello <jcabelloc@gmail.com>
2018-03-30 10:39:02 -0500

    Associate User and Campgrounds + Protecting Campgrounds isLoggedIn

--- routes/campogrounds.js
+++ routes/campogrounds.js
@@ -13,21 +13,27 @@ router.get("/", function(req, res){
     });
 });
 // CREATE - add new campground
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
-    var newCampground = {name: name, image: image, description: description};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newCampground = {name: name, image: image, description: description, author: author};
+    
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
             console.log(err);
         } else {
+            console.log(newlyCreated);
             res.redirect("/campgrounds");
         }
     });
 });
 // NEW -show form to create new campground
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
     res.render("campgrounds/new");
 });
 // SHOW - shows more info about one campground
@@ -41,5 +47,13 @@ router.get("/:id", function(req, res){
         }
     });
 });
+
+// Middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+};
 
 module.exports = router;
