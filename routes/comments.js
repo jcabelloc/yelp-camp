9bb21068355a53bd9cbd6813900faa4b80cc10d5
Juan Cabello <jcabelloc@gmail.com>
2018-03-30 13:31:53 -0500

    Editing comments

--- routes/comments.js
+++ routes/comments.js
@@ -41,6 +41,32 @@ router.post("/", isLoggedIn, function(req, res){
     });
 });
 
+// Comment Edit Route
+router.get("/:comment_id/edit", function(req, res){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+        if(err){
+            res.redirect("back");
+        } else {
+            // req.params.id refers to the campground id
+            res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+        }
+    });
+
+});
+
+// comment update
+router.put("/:comment_id", function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+        if(err){
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+
+
 // Middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
