b20840fbf01f94bbbb40cd938a499cfdbb5044ab
Juan Cabello <jcabelloc@gmail.com>
2018-04-01 12:10:17 -0500

    add mLab DB Connection

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelpDB");
+//mongoose.connect("mongodb://localhost/yelpDB");
+mongoose.connect("mongodb://app:app123@ds113179.mlab.com:13179/yelpdatabase");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
